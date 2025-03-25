0240b2cd463f969e3c0f21b8bda4feb57ddc81b5
unknown <alexvdbr6@gmail.com>
2025-03-25 14:56:22 +0100

    routes ok

--- App/Back(Django)/backend/settings.py
+++ App/Back(Django)/backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Narratica'
+    'Narratica',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
