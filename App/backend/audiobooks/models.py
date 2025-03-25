657b2b7f195a2c2259dbe12245294f9af19b95de
Denis Collette <denis.collette92@gmail.com>
2025-03-25 11:55:31 +0100

    last version

--- App/backend/audiobooks/models.py
+++ App/backend/audiobooks/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 # USER MODEL (Extending Django's Built-in User)
 class User(AbstractUser):
--- App/backend/backend/settings.py
+++ App/backend/backend/settings.py
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DEFAULT SETTINGS:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "narratica-db",
+        "USER": "postgres",
+        "PASSWORD": "hamburger",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
