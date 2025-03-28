44cbec3711181e6ba45c1bcf0542e8c16c8d3581
Hugo0O3 <hugomancini2.0@gmail.com>
2025-03-28 15:01:23 +0100

    ajout shadcn

--- App/Front(React)/narratica/src/app/page.tsx
+++ App/Front(React)/narratica/src/app/page.tsx
@@ -1,7 +1,23 @@
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+
 export default function HomePage() {
     return (
         <section>
             <p>Ceci est la page d'accueil</p>
+            <Carousel>
+                <CarouselContent>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">1</CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">2</CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">3</CarouselItem>
+                </CarouselContent>
+            </Carousel>
+
         </section>
     )
 }  
