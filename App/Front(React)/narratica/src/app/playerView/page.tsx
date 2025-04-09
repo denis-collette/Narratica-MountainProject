5f6d3a1a259f11591f6cdff80e992c61bc73a687
Hugo0O3 <hugomancini2.0@gmail.com>
2025-04-09 10:26:09 +0200

    ajout audioPlayer basique + suppression fichiers pas utile audioPlayer

--- App/Front(React)/narratica/src/app/playerView/page.tsx
+++ App/Front(React)/narratica/src/app/playerView/page.tsx
@@ -2,17 +2,18 @@
 import NavBar from "@/components/NavBar"
 import { useEffect, useState } from 'react';
 import ChapterCard from "@/components/chapterCard";
-import { fetchAudioBooksChapterById , Chapter} from '../api/audio/getChapterById';
+import { fetchAudioBooksChapterById, Chapter } from '../api/audio/getChapterById';
 import { fetchAudioBooksById } from '../api/audio/getAudioBooksById';
 import { Audiobook } from '../api/audio/getAllAudioBooks';
 import { Author, fetchAuthorById } from "../api/audio/getAuthorById";
-import  { Narrator,fetchNarratorById} from '../api/audio/getNarratorById'
+import { Narrator, fetchNarratorById } from '../api/audio/getNarratorById'
 import { useColor } from 'color-thief-react'
 import * as React from 'react'
+import Player from "@/components/audio/custom/Player";
 
 
-function PlayerView({searchParams} : {searchParams : {bookId : string, chapterId : string}}) {
-    
+function PlayerView({ searchParams }: { searchParams: { bookId: string, chapterId: string } }) {
+
     interface Informations {
         chapter: Chapter[];
         audiobook: Audiobook[];
@@ -27,94 +28,99 @@ function PlayerView({searchParams} : {searchParams : {bookId : string, chapterId
     let chapterId = 1
 
     //Check if their is parameters
-    try{
-        if(parseInt(searchParams.bookId)> 0 && parseInt(searchParams.chapterId)> 0){
+    try {
+        if (parseInt(searchParams.bookId) > 0 && parseInt(searchParams.chapterId) > 0) {
             bookId = parseInt(searchParams.bookId)
             chapterId = parseInt(searchParams.chapterId)
         }
-    
-    }catch(e){
+
+    } catch (e) {
         console.log(e)
     }
-    
-    console.log(searchParams.bookId + " " + searchParams.chapterId )
-    
-        const [informations, setState] = useState<Informations>({
-            chapter: [],
-            audiobook: [],
-            author: [],
-            narrator:[],
-            loadingAudioBook: true,
-            loadingChapter: true,
-        });
-    
-        const { data, loading, error } = useColor(informations.audiobook[0]?.cover_art_jpg ?? null, 'hslString',{crossOrigin : "anonymous"})
-        
-        useEffect(() => {
-            const loadData = async () => {
-                try {
-    
-                    let chapter = await fetchAudioBooksChapterById(bookId , chapterId); 
 
-    
-                    let audiobook = await fetchAudioBooksById(bookId);
-                    
-                    if (audiobook && audiobook.length > 0) {
-                        let author = await fetchAuthorById(audiobook[0]?.author);
-                        let narrator = await fetchNarratorById(audiobook[0]?.narrator);
-    
-                        setState((prevState) => ({
-                            ...prevState,
-                                author,
-                                narrator,
-                        }));
-                        
-                    }
-    
-                    
+    console.log(searchParams.bookId + " " + searchParams.chapterId)
+
+    const [informations, setState] = useState<Informations>({
+        chapter: [],
+        audiobook: [],
+        author: [],
+        narrator: [],
+        loadingAudioBook: true,
+        loadingChapter: true,
+    });
+
+    const { data, loading, error } = useColor(informations.audiobook[0]?.cover_art_jpg ?? null, 'hslString', { crossOrigin: "anonymous" })
+
+    useEffect(() => {
+        const loadData = async () => {
+            try {
+
+                let chapter = await fetchAudioBooksChapterById(bookId, chapterId);
+
+
+                let audiobook = await fetchAudioBooksById(bookId);
+
+                if (audiobook && audiobook.length > 0) {
+                    let author = await fetchAuthorById(audiobook[0]?.author);
+                    let narrator = await fetchNarratorById(audiobook[0]?.narrator);
+
                     setState((prevState) => ({
                         ...prevState,
-                        chapter: chapter,
-                        loadingChapter: false,
-                        audiobook,
+                        author,
+                        narrator,
                     }));
-                } catch (error) {
-                    console.error("Error loading data:", error);
+
                 }
-            };
-        
-            loadData();
-        }, [chapterId]);
-        
-        const covertArt = informations.audiobook[0]?.cover_art_jpg  ?? null
-        console.log(JSON.stringify(informations.chapter[0]))
 
 
-return(
+                setState((prevState) => ({
+                    ...prevState,
+                    chapter: chapter,
+                    loadingChapter: false,
+                    audiobook,
+                }));
+            } catch (error) {
+                console.error("Error loading data:", error);
+            }
+        };
 
-    <div className="flex h-screen w-screen">
-        <div className="flex items-center justify-center bg-red-500 w-1/2 h-full ">
-            <div className="bg-blue-500 w-[70%] h-0 pb-[70%] mr-[5%]">
-                <img src={covertArt ?? null} alt="cover art"></img>
-            </div>
-        </div>
-        <div className="flex items-center justify-center bg-green-500 w-1/2 h-full ">
-            <div className="flex-col text-center bg-blue-500 w-[70%] h-0 pb-[70%] mr-[5%]">
+        loadData();
+    }, [chapterId]);
 
-                <div>
-                <div className='text-white text-[1.5em] font-bold'>{informations.audiobook[0]?.title}</div>
-                <div className="text-[hsl(0,_0%,_70%)] text-xs">{informations.chapter[0]?.chapter_number} </div>
-                <div className="text-[hsl(0,_0%,_70%)] text-xs">{informations.author[0]?.name}</div>
-                <div className="text-[hsl(0,_0%,_70%)] text-xs">{informations.narrator[0]?.name}</div>
+    const covertArt = informations.audiobook[0]?.cover_art_jpg ?? null
+    console.log(JSON.stringify(informations.chapter[0]))
+
+
+    return (
+
+        <div className="flex h-screen w-screen">
+            <div className="flex items-center justify-center bg-red-500 w-1/2 h-full ">
+                <div className="bg-blue-500 w-[70%] h-0 pb-[70%] mr-[5%]">
+                    <img src={covertArt ?? null} alt="cover art"></img>
                 </div>
-                
-                <div>
+            </div>
+            <div className="flex items-center justify-center bg-green-500 w-1/2 h-full ">
+                <div className="flex-col text-center bg-blue-500 w-[70%] h-0 pb-[70%] mr-[5%]">
+
+                    <div>
+                        <div className='text-white text-[1.5em] font-bold'>{informations.audiobook[0]?.title}</div>
+                        <div className="text-[hsl(0,_0%,_70%)] text-xs">{informations.chapter[0]?.chapter_number} </div>
+                        <div className="text-[hsl(0,_0%,_70%)] text-xs">{informations.author[0]?.name}</div>
+                        <div className="text-[hsl(0,_0%,_70%)] text-xs">{informations.narrator[0]?.name}</div>
+                    </div>
+
+                    {/* <div>
                 <div className="bg-fuchsia-600">PLAYER</div>
-                </div>
+                </div> */}
 
+                    {informations.chapter[0]?.audio_data && (
+                        <Player audioSource={informations.chapter[0].audio_data} />
+                    )}
+
+                </div>
             </div>
         </div>
-    </div>
-)}
+    )
+}
 
 export default PlayerView
--- App/Front(React)/narratica/src/components/audio/AudioPlayer.tsx
+++ App/Front(React)/narratica/src/components/audio/AudioPlayer.tsx
@@ -1,155 +1,17 @@
-
-"use client"
-import React, { useState, useEffect, useRef } from "react";
-import Controls from "./Controls";
-import Backdrop from "./Backdrop";
-
-interface Track {
-    title: string;
-    artist: string;
-    color: string;
-    image: string;
-    audioSrc: string;
+import React from 'react'
+type AudioPlayerProps = {
+    src: string;
 }
 
-interface AudioPlayerProps {
-    tracks: Track[];
-}
-
-const AudioPlayer: React.FC<AudioPlayerProps> = ({ tracks }) => {
-    const [trackIndex, setTrackIndex] = useState<number>(0);
-    const [trackProgress, setTrackProgress] = useState<number>(0);
-    const [isPlaying, setIsPlaying] = useState<boolean>(false);
-
-    const { title, artist, color, image, audioSrc } = tracks[trackIndex];
-
-    const audioRef = useRef<HTMLAudioElement>(new Audio(audioSrc));
-    const intervalRef = useRef<NodeJS.Timeout | null>(null);
-    const isReady = useRef<boolean>(false);
-
-    const { duration } = audioRef.current;
-
-    const currentPercentage = duration
-        ? `${(trackProgress / duration) * 100}%`
-        : "0%";
-    const trackStyling = `
-    -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))
-  `;
-
-    const startTimer = () => {
-        if (intervalRef.current) {
-            clearInterval(intervalRef.current);
-        }
-
-        intervalRef.current = setInterval(() => {
-            if (audioRef.current.ended) {
-                toNextTrack();
-            } else {
-                setTrackProgress(audioRef.current.currentTime);
-            }
-        }, 1000);
-    };
-
-    const onScrub = (value: number) => {
-        if (intervalRef.current) {
-            clearInterval(intervalRef.current);
-        }
-        audioRef.current.currentTime = value;
-        setTrackProgress(audioRef.current.currentTime);
-    };
-
-    const onScrubEnd = () => {
-        if (!isPlaying) {
-            setIsPlaying(true);
-        }
-        startTimer();
-    };
-
-    const toPrevTrack = () => {
-        if (trackIndex - 1 < 0) {
-            setTrackIndex(tracks.length - 1);
-        } else {
-            setTrackIndex(trackIndex - 1);
-        }
-    };
-
-    const toNextTrack = () => {
-        if (trackIndex < tracks.length - 1) {
-            setTrackIndex(trackIndex + 1);
-        } else {
-            setTrackIndex(0);
-        }
-    };
-
-    useEffect(() => {
-        if (isPlaying) {
-            audioRef.current.play();
-            startTimer();
-        } else {
-            audioRef.current.pause();
-        }
-    }, [isPlaying]);
-
-    useEffect(() => {
-        audioRef.current.pause();
-
-        audioRef.current = new Audio(audioSrc);
-        setTrackProgress(audioRef.current.currentTime);
-
-        if (isReady.current) {
-            audioRef.current.play();
-            setIsPlaying(true);
-            startTimer();
-        } else {
-            isReady.current = true;
-        }
-    }, [trackIndex]);
-
-    useEffect(() => {
-        return () => {
-            audioRef.current.pause();
-            if (intervalRef.current) {
-                clearInterval(intervalRef.current);
-            }
-        };
-    }, []);
-
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ src }) => {
     return (
-        <div className="audio-player">
-            <div className="track-info">
-                <img
-                    className="artwork"
-                    src={image}
-                    alt={`track artwork for ${title} by ${artist}`}
-                />
-                <h2 className="title">{title}</h2>
-                <h3 className="artist">{artist}</h3>
-                <Controls
-                    isPlaying={isPlaying}
-                    onPrevClick={toPrevTrack}
-                    onNextClick={toNextTrack}
-                    onPlayPauseClick={setIsPlaying}
-                />
-                <input
-                    type="range"
-                    value={trackProgress}
-                    step="1"
-                    min="0"
-                    max={duration ? duration : `${duration}`}
-                    className="progress"
-                    onChange={(e) => onScrub(Number(e.target.value))}
-                    onMouseUp={onScrubEnd}
-                    onKeyUp={onScrubEnd}
-                    style={{ background: trackStyling }}
-                />
-            </div>
-            <Backdrop
-                trackIndex={trackIndex}
-                activeColor={color}
-                isPlaying={isPlaying}
-            />
-        </div>
-    );
-};
+        <section className="p-5 bg-gray-300 rounded">
+            <audio controls>
+                <source src={src}/>
+                Test audio
+            </audio>
+        </section>
+    )
+}
 
-export default AudioPlayer;
+export default AudioPlayer
