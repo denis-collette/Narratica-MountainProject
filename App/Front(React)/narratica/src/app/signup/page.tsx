f16e4881ca683e24babb86c3919c3aa9129be6f9
Alex <alexvdbr6@gmail.com>
2025-04-09 16:17:31 +0200

    working form

--- App/Front(React)/narratica/src/app/signup/page.tsx
+++ App/Front(React)/narratica/src/app/signup/page.tsx
@@ -8,11 +8,31 @@ function SignUpView(){
         console.log(JSON.stringify(info))
     }
 
+    const handleSubmit = (e : any) =>  {
+            e.preventDefault(); 
+            const formData = new FormData(e.currentTarget) 
+            const userName = formData.get("pseudo")
+            const password = formData.get("password")
+            const first_name = formData.get("fname")
+            const last_name = formData.get("lname")
+            const email = formData.get("email")
+
+            const apiObj = {
+                "user_name" : userName,
+                "first_name" : first_name,
+                "last_name" : last_name,
+                "email" : email,
+                "password" : password
+            }
+            console.log(apiObj)
+    }
+
+
     return(
-        
+                
         <main>
             <div className="flex justify-center items-center min-h-screen">
-            <form className="space-y-4" action="/">
+            <form className="space-y-4" onSubmit={handleSubmit}>
                 <label htmlFor="firstname" className="text-white">First name:</label><br />
                 <input className="text-white border-2 divide-gray-50" type="text" id="fname" name="fname" /><br />
                 
@@ -28,7 +48,7 @@ function SignUpView(){
                 <label className="text-white">Password:</label><br />
                 <input className="text-white border-2 divide-gray-50" type="password" id="password" name="password" /><br />
                 
-                <input className="text-white border-2 divide-gray-50" type="submit" value="Submit" />
+                <button className="text-white border-2 divide-gray-50" type="submit" value="Submit">Submit</button>
             </form>
             </div>
         </main>
