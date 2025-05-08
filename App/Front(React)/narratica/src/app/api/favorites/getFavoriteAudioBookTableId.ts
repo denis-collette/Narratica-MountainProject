8abe70ed6df244b9f1cf9b218d3f80d84e97a15f
Denis Collette <denis.collette92@gmail.com>
2025-05-08 14:55:00 +0200

    token fixed for favorites

--- App/Front(React)/narratica/src/app/api/favorites/getFavoriteAudioBookTableId.ts
+++ App/Front(React)/narratica/src/app/api/favorites/getFavoriteAudioBookTableId.ts
@@ -20,8 +20,19 @@ export const fetchFavoriteAudioBookTableId = async (linkTableQuery : LinkTableQu
     let routeUrl = url + `api/favorites/books/?user=${linkTableQuery.user}&book=${linkTableQuery.book}`
     
     try {
-        const response = await axios.get<LinkTable[]>(routeUrl);
-        return response.data;
+        const token = localStorage.getItem("access");
+        const response = await fetch(routeUrl, { 
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data: LinkTable[] = await response.json();
+        return data;
     } catch (error) {
         console.error('Error fetching data:', error);
         return [];
